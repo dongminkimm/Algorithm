745735d6d01e9c6bd2d60add155ede967356e8cd
dongminkimm <kdm753@gmail.com>
2024-08-15 16:52:43 +0900

    0815

--- temp.py
+++ temp.py
@@ -36,34 +36,3 @@
 #     print(cnt)
 
 
-# 14940 쉬운 최단거리
-from collections import deque
-
-n, m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
-result = [[0] * m for _ in range(n)]
-
-for i in range(len(graph)):
-    if 2 in graph[i]:
-        j = graph[i].index(2)
-        x, y = i, j
-        break
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
-
-q = deque([(x, y)])
-while q:
-    x, y = q.popleft()
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if nx >= 0 and ny >= 0 and nx < n and ny < m:
-            if graph[nx][ny] == 1 and result[nx][ny] == 0:
-                result[nx][ny] = result[x][y] + 1
-                graph[nx][ny] = 0
-                q.append((nx, ny))
-
-for i in result:
-    for j in i:
-        print(j, end=' ')
-    print()
